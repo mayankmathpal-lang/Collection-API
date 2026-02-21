c9138db2088427f07564528fc4295d3248dbab55
Mayank Mathpal <mayankmathpal939@gmail.com>
2026-02-21 20:46:06 +0530

    first commit

---
+++ LaunchArrayDeque.java
@@ -0,0 +1,15 @@
+import java.util.ArrayDeque;
+public class LaunchArrayDeque {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          ArrayDeque ad = new ArrayDeque();
+          ad.add(10);
+          ad.add(20);
+          ad.add(30);
+          ad.add(40);
+         System.out.println(ad);
+         
+	}
+
+}
